c20d36dab6a2c83b4396335acbaf977ba03c5235
Temp1sachin <sachin.mishra2442@gmail.com>
2025-02-01 03:24:31 +0530

    chat added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Editor from '../components/editor';
 import HomeComponent from '../components/Home';
+import Chat from "../components/Chat";
 
 export default function Home() {
   return (
     <div>
       <HomeComponent/>
       <Editor />
+      
     </div>
   );
 }
